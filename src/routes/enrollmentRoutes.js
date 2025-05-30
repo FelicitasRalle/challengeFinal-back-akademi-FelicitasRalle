8f90053588d007b57c38f1120e4e23f7dd899468
Felicitas Ralle <felicitasralle@gmail.com>
2025-05-30 12:54:14 -0300

    feat: añadir la ruta POST en enrollmentRoutes

--- src/routes/enrollmentRoutes.js
+++ src/routes/enrollmentRoutes.js
@@ -1,27 +1,45 @@
-const express = require("express");
+const express = require('express');
+const { body } = require('express-validator');
 const {
   getEnrollmentsByStudent,
   createEnrollment,
   deleteEnrollment,
-  getEnrollmentsByCourse,
+  getEnrollmentsByCourse
 } = require('../controllers/enrollmentController');
 const { protect, restrictTo } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-//proteccion a todas las rutas
 router.use(protect);
 
+//POST /enrollments
+router.post(
+  '/',
+  restrictTo('student'),
+  body('courseId')
+    .notEmpty().withMessage('courseId es obligatorio')
+    .isMongoId().withMessage('courseId debe ser un ID válido'),
+  createEnrollment
+);
 
+//GET /enrollments/student/:studentId
 router.get(
-  "/student/:studentId",
-  restrictTo("student"),
+  '/student/:studentId',
+  restrictTo('student'),
   getEnrollmentsByStudent
 );
-router.delete("/:id", restrictTo("student"), deleteEnrollment);
+
+//DELETE /enrollments/:id
+router.delete(
+  '/:id',
+  restrictTo('student'),
+  deleteEnrollment
+);
+
+// GET /enrollments/course/:courseId
 router.get(
-  "/course/:courseId",
-  restrictTo("professor"),
+  '/course/:courseId',
+  restrictTo('professor'),
   getEnrollmentsByCourse
 );
 

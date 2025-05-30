403d545ab3863d0d25d23245cb4ddf6647b92fab
Felicitas Ralle <felicitasralle@gmail.com>
2025-05-30 10:14:02 -0300

    fix: corregir nombres de rutas mal escritas

--- src/routes/coursesRoutes.js
+++ src/routes/coursesRoutes.js
@@ -1,28 +1,25 @@
-const express = require("express");
+// src/routes/coursesRoutes.js
+const express = require('express');
 const {
   getCourses,
-  getCourseById,
+  getCoursesById,
   createCourse,
   updateCourse,
   deleteCourse,
-  getCoursesByProfessor,
-} = require("../controllers/courseController");
-
-const { protect, restrictTo } = require("../middleware/authMiddleware");
+  getCoursesByProfessor
+} = require('../controllers/courseController');
+const { protect, restrictTo } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+//proteccion a todas las rutas
 router.use(protect);
 
-router.get("/", restrictTo("student"), getCourses);
-router.get("/:id", getCourseById);
-router.post("/", restrictTo("professor"), createCourse);
-router.put("/:id", restrictTo("professor"), updateCourse);
-router.delete("/:id", restrictTo("professor"), deleteCourse);
-router.get(
-  "/professor/:professorId",
-  restrictTo("professor"),
-  getCoursesByProfessor
-);
+router.get('/', restrictTo('student'), getCourses);
+router.get('/:id', getCoursesById);
+router.post('/', restrictTo('professor'), createCourse);
+router.put('/:id', restrictTo('professor'), updateCourse);
+router.delete('/:id', restrictTo('professor'), deleteCourse);
+router.get('/professor/:professorId', restrictTo('professor'), getCoursesByProfessor);
 
 module.exports = router;
--- src/routes/enrollmentRoutes.js
+++ src/routes/enrollmentRoutes.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const{
-    getEnrollmentByStudent,
-    createEnrollment,
-    deleteEnrollment,
-    getEnrollmentByCourse
+const express = require("express");
+const {
+  getEnrollmentsByStudent,
+  createEnrollment,
+  deleteEnrollment,
+  getEnrollmentsByCourse,
 } = require('../controllers/enrollmentController');
 const { protect, restrictTo } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 //proteccion a todas las rutas
-router.get('/student/:studentId', restrictTo('student'), getEnrollmentByStudent);
-router.post('/', restrictTo('student'), createEnrollment);
-router.delete('/:id', restrictTo('student'), deleteEnrollment);
-router.get('/course/:courseId', restrictTo('professor'), getEnrollmentByCourse);
+router.use(protect);
+
+
+router.get(
+  "/student/:studentId",
+  restrictTo("student"),
+  getEnrollmentsByStudent
+);
+router.delete("/:id", restrictTo("student"), deleteEnrollment);
+router.get(
+  "/course/:courseId",
+  restrictTo("professor"),
+  getEnrollmentsByCourse
+);
 
 module.exports = router;

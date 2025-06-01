7c2da4a1b5c6f1b699bddea26d32bb6c261fcf08
Felicitas Ralle <felicitasralle@gmail.com>
2025-06-01 20:05:44 -0300

    fix: arreglar el try del middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,31 +1,36 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 //verificar q existe el token y lo pasa a decodificar
-exports.protect = async (req, res, next) =>{
-    let token;
-    const authHeader = req.headers.authorization;
-    if(authHeader && authHeader.startsWith('Bearer')){
-        token = authHeader.split(' ')[1];
-    }
-    if(!token){
-        return res.status(401).json({ message: 'No se autorizo, token incorrecto o faltante' });
-    }
+exports.protect = async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "No se autorizo, token incorrecto o faltante" });
+  }
 
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        next();
-    }catch(error){
-        return res.status(401).json({ message: 'Token invalido' });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password"); 
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token invalido" });
+  }
 };
 
 //restringir roles
-exports.restrictTo = (...roles) =>{
-    return (req, res, next)=>{
-        if (!roles.includes(req.user.role)){
-            return res.status(403).json({ message: 'Permiso no autorizado para este rol'});
-        }
-        next();
-    };
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Permiso no autorizado para este rol" });
+    }
+    next();
+  };
 };
